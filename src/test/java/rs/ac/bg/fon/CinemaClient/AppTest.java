18d936a2714ac2ae7a972657c099381ba7df4e02
nikolaabadic <nikolaabadic@gmail.com>
2021-06-14 16:18:32 +0200

    Project initialization.

---
+++ src/test/java/rs/ac/bg/fon/CinemaClient/AppTest.java
@@ -0,0 +1,20 @@
+package rs.ac.bg.fon.CinemaClient;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
